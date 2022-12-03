e91a83d42b362edf76ddfef08007778197ecc43f
trembon <trembon@hotmail.com>
2022-12-03 09:58:04 +0100

    day 3

---
+++ AdventOfCode2022/Day03/ProblemSolver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day03
+{
+    internal class ProblemSolver : IProblemSolver
+    {
+        public Task<double> SolveTaskOne(string[] input)
+        {
+            var sharedItems = FindSharedItems(input);
+            double priority = CalculatePriority(sharedItems);
+            return Task.FromResult(priority);
+        }
+
+        public Task<double> SolveTaskTwo(string[] input)
+        {
+            var sharedItems = FindSharedItemsInThreeGroups(input);
+            double priority = CalculatePriority(sharedItems);
+            return Task.FromResult(priority);
+        }
+
+        private List<char> FindSharedItems(string[] input)
+        {
+            List<char> sharedItems = new(input.Length);
+            foreach(string comp in input)
+            {
+                string first = comp.Substring(0, comp.Length / 2);
+                string second = comp.Substring(comp.Length / 2);
+
+                IEnumerable<char> shared = first.Where(x => second.Contains(x));
+                sharedItems.Add(shared.First());
+            }
+            return sharedItems;
+        }
+
+        private double CalculatePriority(List<char> input)
+        {
+            double priority = 0;
+            foreach(char c in input)
+            {
+                if (Char.IsUpper(c))
+                    priority += (int)c - 64 + 26;
+                else if(Char.IsLower(c))
+                    priority += (int)c - 96;
+            }
+            return priority;
+        }
+
+        private List<char> FindSharedItemsInThreeGroups(string[] input)
+        {
+            List<char> sharedItems = new(input.Length / 3);
+            for (int i = 0; i < input.Length; i += 3)
+            {
+                string first = input[i];
+                string second = input[i + 1];
+                string third = input[i + 2];
+
+                IEnumerable<char> shared = first.Where(x => second.Contains(x) && third.Contains(x));
+                sharedItems.Add(shared.First());
+            }
+            return sharedItems;
+        }
+    }
+}
